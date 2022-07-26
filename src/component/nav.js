7637980313f8ec2383e63295d9750a1cd34bd715
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-27 02:01:58 +0530

    add

---
+++ src/component/nav.js
@@ -0,0 +1,46 @@
+function Nav() {
+  return (
+    <div className="Container">
+      <div className="row">
+        <div className="col-md-6 text-center">
+          <h1>Newton School</h1>
+          <p>
+            Even so my sun one early morn did shine, With all triumphant
+            spleandour on my brow; But out, alack, he was but one hour mine, The
+            region cloud hath mask’d him from me now … ‘Full many a glorious
+            morning have I seen’: Sonnet 33 is, without doubt, one of the more
+            famous of Shakespeare’s Sonnets. It is also one of the most
+            beautiful poems in English. The extended metaphor whereby ‘sun =
+            Fair Youth’ in this poem is intended to pay homage to the young
+            man’s beauty: he shines as brightly as that heavenly orb.
+            Shakespeare piles on the flattery, though, with extra touches: the
+            sun has a ‘sovereign eye’ and so, by association, has the Fair Youth
+            – ‘sovereign’ suggesting royalty or at least nobility. The words
+            ‘golden’, ‘gilding’, and ‘alchemy’ all reinforce this association
+            with wealth and nobility.
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <h1>Nawaz Danish</h1>
+          <p>
+            Even so my sun one early morn did shine, With all triumphant
+            splendour on my brow; But out, alack, he was but one hour mine, The
+            region cloud hath mask’d him from me now … ‘Full many a glorious
+            morning have I seen’: Sonnet 33 is, without doubt, one of the more
+            famous of Shakespeare’s Sonnets. It is also one of the most
+            beautiful poems in English. The extended metaphor whereby ‘sun =
+            Fair Youth’ in this poem is intended to pay homage to the young
+            man’s beauty: he shines as brightly as that heavenly orb.
+            Shakespeare piles on the flattery, though, with extra touches: the
+            sun has a ‘sovereign eye’ and so, by association, has the Fair Youth
+            – ‘sovereign’ suggesting royalty or at least nobility. The words
+            ‘golden’, ‘gilding’, and ‘alchemy’ all reinforce this association
+            with wealth and nobility.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
